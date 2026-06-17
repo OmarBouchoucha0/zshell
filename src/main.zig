8ba477e53f29d0e6d33a6547b9c24dfafeb329a3
OmarBouchoucha0 <bouchouchaomar38@gmail.com>
2026-06-17 20:15:37 +0100

    exit

--- src/main.zig
+++ src/main.zig
@@ -22,12 +22,24 @@ pub fn main(init: std.process.Init) !void {
 
         var reader = std.Io.File.reader(stdin, io, read_buffer);
         const n: usize = try reader.interface.discardDelimiterExclusive('\n');
-
-        try writer.interface.print("cmd : {s}\n", .{read_buffer[0..n]});
+        const output = try handler(allocator, read_buffer[0..n]);
+        try writer.interface.print("{s}\n", .{output});
         try writer.interface.flush();
     }
 }
 
-// pub fn handler(cmd: []u8) ![]u8 {
-//     std.debug.print("not implemented");
-// }
+const BuiltinCmd = enum {
+    exit,
+};
+
+pub fn handler(allocator: std.mem.Allocator, cmd: []const u8) ![]const u8 {
+    const output: []u8 = try allocator.alloc(u8, cmd.len);
+    const lower_cmd = std.ascii.lowerString(output, cmd);
+    const cmd_enum = std.meta.stringToEnum(BuiltinCmd, lower_cmd) orelse {
+        const warning_msg: []const u8 = "Unknown command";
+        return warning_msg;
+    };
+    switch (cmd_enum) {
+        .exit => std.process.exit(0),
+    }
+}
