c8de460cf9a11dc97162859dd03d6d9a9ba8cba8
OmarBouchoucha0 <bouchouchaomar38@gmail.com>
2026-06-18 13:26:19 +0100

    type cmd

--- src/main.zig
+++ src/main.zig
@@ -23,9 +23,12 @@ pub fn main(init: std.process.Init) !void {
 
         var reader = std.Io.File.reader(stdin, io, read_buffer);
         const n: usize = try reader.interface.discardDelimiterExclusive('\n');
-        const output = try handler(allocator, read_buffer[0..n]);
-        try writer.interface.print("{s}\n", .{output});
-        try writer.interface.flush();
+        const whitespace = " \t\n\r";
+        if (std.mem.indexOfNone(u8, read_buffer[0..n], whitespace) != null) {
+            const output = try handler(allocator, io, read_buffer[0..n]);
+            try writer.interface.print("{s}", .{output});
+            try writer.interface.flush();
+        }
     }
 }
 
@@ -43,22 +46,31 @@ const Input = struct {
 const BuiltinCmd = enum {
     exit,
     echo,
+    type,
 };
 
-pub fn handler(allocator: std.mem.Allocator, input: []const u8) ![]const u8 {
+pub fn handler(allocator: std.mem.Allocator, io: std.Io, input: []const u8) ![]const u8 {
     const output: []u8 = try allocator.alloc(u8, input.len);
-    const input_lower_case = std.ascii.lowerString(output, input);
-    const input_parsed = parseInput(input_lower_case);
+    const input_parsed = parseInput(input);
     var cmd_output: []const u8 = undefined;
-    const cmd_enum = std.meta.stringToEnum(BuiltinCmd, input_parsed.cmd) orelse {
-        const warning_msg: []const u8 = "Unknown command";
-        return warning_msg;
+    const cmd_lower_case = std.ascii.lowerString(output, input_parsed.cmd);
+    const cmd_enum = std.meta.stringToEnum(BuiltinCmd, cmd_lower_case) orelse {
+        const result = std.process.run(allocator, io, .{
+            .argv = &.{ input_parsed.cmd, input_parsed.args },
+        }) catch {
+            const warning_msg: []const u8 = "Unknown command \n";
+            return warning_msg;
+        };
+        return result.stdout;
     };
 
     switch (cmd_enum) {
         .exit => exit(),
         .echo => {
             cmd_output = echo(input_parsed.args);
+        },
+        .type => {
+            cmd_output = try ttype(allocator, input_parsed.args);
         },
     }
     return cmd_output;
@@ -84,4 +96,12 @@ pub fn exit() void {
 
 pub fn echo(args: []const u8) []const u8 {
     return args;
+}
+
+pub fn ttype(allocator: std.mem.Allocator, args: []const u8) ![]const u8 {
+    const buffer: []u8 = try allocator.alloc(u8, 100);
+    _ = std.meta.stringToEnum(BuiltinCmd, args) orelse {
+        return std.fmt.bufPrint(buffer, "{s} is not found\n", .{args});
+    };
+    return std.fmt.bufPrint(buffer, "{s} is a shell builtin\n", .{args});
 }
